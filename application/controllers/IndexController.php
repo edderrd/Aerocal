2358d81ee6438861c56c6f20a4ed4f9933ee1e22
Edder Rojas Douglas <edder.rojas@gmail.com>
2010-05-13 18:09:13 -0600

    - Added a class to generate calendar options - Added missing view scripts - Bump application version to 0.2

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,8 @@ class IndexController extends App_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $fc = new App_FullCalendar();
+        $this->view->calendarOptions = $fc->getConfig();
     }
 
 
