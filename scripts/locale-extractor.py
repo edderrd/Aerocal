a5c4a755f40185b3dac0a75b84ebf63f380a7936
Edder Rojas <edder@cerberus.(none)>
2010-05-27 20:08:52 -0600

    - Better script creation and now update all language files, without   overriding translation allready there

--- scripts/locale-extractor.py
+++ scripts/locale-extractor.py
@@ -1,14 +1,14 @@
-#!/usr/env python
+#!/usr/bin/env python
 
 import os
-import stat
 import re
-import getopt
 import sys
     
 class PhrasesFinder:
     
     files = list();
+    languageFiles = list()
+    languageFileExtension = "csv"
     
     def __init__(self, directoryList, fileExtensions):
         assert type(directoryList)==list
@@ -19,7 +19,7 @@ class PhrasesFinder:
         self.files = self._getFiles();
         self.pattern = '_\([a-zA-Z_0-9 "\']+\)'
         self.progPattern = re.compile(self.pattern)
-        
+        self.languageFolder = self.directoryList[0].replace("/application", "") + 'languages'
 
     def _getFiles(self):
         rv = list();
@@ -52,11 +52,33 @@ class PhrasesFinder:
             return rv.keys();
         return list()
 
+    def getLanguageFiles(self):
+        self.languageFiles = list()
+        f = os.popen(" cd "+self.languageFolder+"; find -iname '*."+self.languageFileExtension+"'")
+        for i in f:
+            if i is not "":
+                if not i.startswith("./en"):
+                    self.languageFiles.append(i.replace("\n", "").replace("./", self.languageFolder+"/"))
+        return self.languageFiles
+
+
 class FileHandler:
     
-    def __init__(self, filePath, ):
+    def __init__(self, filePath):
         self.filePath = filePath
+
+    def _getExistingPhrases(self, filePath):
+        f = open(filePath, "r+")
+        phrases = dict()
         
+        for line in f:
+            line = line.replace("\n", "")
+            if (line is not ""):
+                word, definition = line.split(";")
+                phrases[word] = definition.replace("\n", "")
+        f.close()
+        return phrases
+
     def saveBasePhrases(self, phrases):
         # TODO combine phares with file
         if phrases:
@@ -64,6 +86,33 @@ class FileHandler:
             for phrase in phrases:
                 f.write(phrase +";"+ phrase + "\n")
             f.close()
+
+    def updateLanguageFiles(self, languageFiles, phrases):
+        if not languageFiles:
+            print "No languange files defined"
+            exit(-1)
+        if not phrases:
+            print "No phrases passed"
+            exit(-1)
+            
+        for languageFile in languageFiles:
+            filePhrases = self._getExistingPhrases(languageFile)
+            f = open(languageFile, "w")
+            
+            if filePhrases:
+                fileWords = filePhrases.keys()
+                
+                for phrase in phrases:
+                    if phrase in fileWords:
+                        f.write(phrase+";"+ filePhrases[phrase]+"\n")
+                    else:
+                        f.write(phrase+";"+phrase+"\n")
+            else:
+                for phrase in phrases:
+                    f.write(phrase+";"+ phrase +"\n")
+            print("File: " + languageFile + " updated")
+            f.close()
+
                    
 if __name__ == "__main__":    
     args = sys.argv
@@ -76,7 +125,9 @@ if __name__ == "__main__":
     phrases = pf.getPhrases()
     print "Found " + str(len(phrases)) + " phrases"
     
-    basicFile = FileHandler(args[1]+'/languages/en/default.csv')
-    basicFile.saveBasePhrases(phrases)
+    fileHandler = FileHandler(args[1]+'/languages/en/default.csv')
+    fileHandler.saveBasePhrases(phrases)
+    fileHandler.updateLanguageFiles(pf.getLanguageFiles(), phrases)
+
     print "Phrases saved on " + args[1]+ "/languages/en/default.csv"
     
